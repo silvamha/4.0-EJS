e317972438a1a5e06a0a3aaac2046ad7fe5b8c2b
Ed <silva.email@yahoo.com>
2024-02-13 11:17:20 -0500

    Add day of the week functionality in index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * 0 - Sunday
+ * 1 - Monday
+ * 2 - Tuesday
+ * 3 - Wednesday
+ * 4 - Thursday
+ * 5 - Friday
+ * 6 - Saturday
+ */
+const d = new Date()
+let day = d.getDay()
+console.log(day)
